547aebf66ed140b3d3ada30fa58e09a7b62aac67
Pop <kppnesh@gmail.com>
2022-05-28 16:33:22 +0530

    Commit from Branch1

--- HowYouDoin.py
+++ HowYouDoin.py
@@ -1,2 +1,4 @@
 print('Hello Friend')
 print('See git is easy!')
+print('Adding new lines in Branch 1')
+print("These lines won't be visible from master branch, unless you merged with master branch")
